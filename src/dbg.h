8054e6be11661f7ff9b6229941a7a2ae8df4a173
treeswift <treeswift@mailbox.org>
2023-10-15 10:58:40 -0500

    auto-terminate log format strings with \n

--- src/dbg.h
+++ src/dbg.h
@@ -2,7 +2,7 @@
 #define _MEMMAP_SRC_DBG_H_
 
 #ifdef _MEMMAP_DEBUG
-#define _MEMMAP_LOG(format, ...) { fprintf(_MEMMAP_DEBUG, format, ##__VA_ARGS__); fflush(_MEMMAP_DEBUG); }
+#define _MEMMAP_LOG(format, ...) { fprintf(_MEMMAP_DEBUG, format "\n", ##__VA_ARGS__); fflush(_MEMMAP_DEBUG); }
 #else
 #define _MEMMAP_LOG(...)
 #endif
